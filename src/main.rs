4a05ef42684b84e4429cdeecf934fe7aa1c21e29
Tzane <taneli.leppanen@hotmail.com>
2024-06-29 22:45:08 +0300

    Progress

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ use lazy_static::lazy_static;
 use rusqlite::Connection;
 use rusqlite_migration::{Migrations, SchemaVersion};
 
+const MIGRATIONS_VERSION: NonZeroUsize = unsafe {NonZeroUsize::new_unchecked(1)};
 static MIGRATIONS_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR/migrations");
 lazy_static! {
     static ref MIGRATIONS: Migrations<'static> =
@@ -25,7 +26,7 @@ fn default_db_path() -> PathBuf {
 }
 
 #[derive(Parser)]
-#[command(version, about, arg_required_else_help = true)]
+#[command(version, about="Log your work hour balance.", arg_required_else_help = true)]
 struct Cli {
     /// Path to database
     #[arg(long, default_value = default_db_path().into_os_string())]
@@ -40,7 +41,13 @@ enum Commands {
     Migrate,
     /// Display database schema information
     Schema,
-    Add,
+    /// Add new entry
+    Add {
+        /// Entry date, "now" or YYYY-MM-DD
+        date: String,
+        /// Hour amount
+        time: f32
+    },
 }
 
 fn migrate(conn: &mut Connection) -> Result<()> {
@@ -55,7 +62,19 @@ fn schema(conn: &Connection) -> Result<()> {
         SchemaVersion::Inside(v) => v.to_string(),
         SchemaVersion::Outside(v) => v.to_string(),
     };
-    println!("Database schema version: {}", &version_str);
+    println!("Database schema version:  {}", &version_str);
+    println!("Latest available version: {}", &MIGRATIONS_VERSION);
+    Ok(())
+}
+
+fn add(conn: &Connection, date: &str, time: f32) -> Result<()> {
+    dbg!(date);
+    dbg!(time);
+    let date_naive = match date {
+        "now" => chrono::offset::Local::now().date_naive(),
+        _ => chrono::NaiveDate::parse_from_str(date, "%Y-%m-%d")?,
+    };
+    dbg!(date_naive);
     Ok(())
 }
 
@@ -72,8 +91,13 @@ fn main() -> Result<()> {
         return Ok(());
     }
 
-    if MIGRATIONS.current_version(&conn)? != SchemaVersion::Inside(NonZeroUsize::new(1).unwrap()) {
+    if MIGRATIONS.current_version(&conn)? != SchemaVersion::Inside(MIGRATIONS_VERSION) {
         return Err(anyhow!("Database is not up to date with the latest schema"));
+    }
+
+    match cli.command {
+        Commands::Add { date, time } => add(&conn, &date, time)?,
+        _ => (),
     }
 
     Ok(())
